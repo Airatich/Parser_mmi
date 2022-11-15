704633932fc55ef039c87fbb8106f49697c5376e
Airatich <106591216+Airatich@users.noreply.github.com>
2022-11-15 18:08:27 +0300

    Update statistics_consumption_4.py

--- statistics_consumption_4.py
+++ statistics_consumption_4.py
@@ -15,7 +15,7 @@ from selenium.webdriver.chrome.service import Service
 url="https://old.metalsmining.ru/"
 options=webdriver.ChromeOptions()
 options.add_argument("--headless")
-options.add_argument("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36")
+options.add_argument("")
 
 s = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=s, options=options)
